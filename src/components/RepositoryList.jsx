1a947d7060c6c227ffa389b0f9206c00e43c5d4d
arkuar <arttukuronen@gmail.com>
2021-02-23 21:17:57 +0200

    10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { useHistory } from 'react-router-native';
 import useRepositories from '../hooks/useRepositories';
 import RepositoryItem from './RepositoryItem';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 
 const styles = StyleSheet.create({
   separator: {
@@ -10,7 +12,40 @@ const styles = StyleSheet.create({
   },
 });
 
-export const RepositoryListContainer = ({ repositories }) => {
+const Dropdown = ({ onSortChange }) => {
+
+  return (
+    <View>
+      <DropDownPicker
+        style={{ padding: 20, height: 50 }}
+        onChangeItem={onSortChange}
+        defaultValue={'latest'}
+        containerStyle={{ height: 50 }}
+        itemStyle={{
+          justifyContent: 'flex-start'
+        }}
+        items={[
+          { label: 'Latest repositories', value: 'latest' },
+          { label: 'Highest rated repositories', value: 'highest' },
+          { label: 'Lowest rated repositories', value: 'lowest' },
+        ]}
+      />
+    </View>
+  );
+};
+
+const sortParams = (active) => {
+  switch (active) {
+    case "highest":
+      return { orderBy: 'RATING_AVERAGE', orderDirection: 'DESC' };
+    case "lowest":
+      return { orderBy: 'RATING_AVERAGE', orderDirection: 'ASC' };
+    default:
+      return { orderBy: 'CREATED_AT', orderDirection: 'DESC' };
+  }
+};
+
+export const RepositoryListContainer = ({ repositories, onSortChange }) => {
   const history = useHistory();
 
   const repositoryNodes = repositories
@@ -22,21 +57,33 @@ export const RepositoryListContainer = ({ repositories }) => {
   };
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => <TouchableOpacity onPress={() => handlePress(item.id)}><RepositoryItem item={item} /></TouchableOpacity>}
-    />
+    <View>
+      <Dropdown onSortChange={onSortChange} />
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => <TouchableOpacity onPress={() => handlePress(item.id)}><RepositoryItem item={item} /></TouchableOpacity>}
+      />
+    </View>
   );
 };
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [sort, setSort] = useState();
+  const { orderBy, orderDirection } = sortParams(sort);
+  const { repositories } = useRepositories({
+    orderBy,
+    orderDirection
+  });
+
+  const onSortChange = ({ value }) => {
+    setSort(value);
+  };
 
   return (
-    <RepositoryListContainer repositories={repositories} />
+    <RepositoryListContainer repositories={repositories} onSortChange={onSortChange} />
   );
 };
 
